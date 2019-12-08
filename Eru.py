ac4ddff8c44a2297b5d79eb16c18e89ef9996397
yimgunho-notebook <rjsgh2119@gmail.com>
2019-12-08 20:45:16 +0900

    ERU 클래스 최적화

--- Eru.py
+++ Eru.py
@@ -8,14 +8,15 @@ import main_state
 
 Eru_Size = 200
 
-WIDTH = 750
-HEIGHT = 900
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+SPEED_KMPH = 10.0  # Km / Hour
+SPEED_MPM = (SPEED_KMPH * 1000.0 / 60.0)
+SPEED_MPS = (SPEED_MPM / 60.0)
+SPEED_PPS = (SPEED_MPS * PIXEL_PER_METER)
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
-
-bullet = []
 
 RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, ONE, TWO, THREE, ZERO, NINE = range(9)
 
@@ -46,40 +47,33 @@ class MoveState:
     @staticmethod
     def enter(eru, event):
         if event == RIGHT_DOWN:
-            eru.velocity += game_world.SPEED_PPS * 5
+            eru.velocity += SPEED_PPS * 5
         elif event == LEFT_DOWN:
-            eru.velocity -= game_world.SPEED_PPS * 5
+            eru.velocity -= SPEED_PPS * 5
         elif event == RIGHT_UP:
-            eru.velocity -= game_world.SPEED_PPS * 5
+            eru.velocity -= SPEED_PPS * 5
         elif event == LEFT_UP:
-            eru.velocity += game_world.SPEED_PPS * 5
+            eru.velocity += SPEED_PPS * 5
 
     @staticmethod
     def exit(eru, event):
         if event == ONE:
-            if eru.bullet_atk_upgrade < 4 and eru.gold >= eru.bullet_atk_upgrade * 10:
-                eru.gold -= eru.bullet_atk_upgrade * 10
-                eru.bullet_atk_upgrade += 1
+            if eru.attack_upgrade_value < 4 and eru.gold >= (eru.attack_upgrade_value + 1) * 10:
+                eru.gold -= (eru.attack_upgrade_value + 1) * 10
+                eru.attack_upgrade_value += 1
 
-        if event == TWO and eru.gold >= eru.bullet_speed_upgrade * 10:
-            if eru.bullet_speed_upgrade < 4:
-                eru.gold -= eru.bullet_speed_upgrade * 10
-                eru.bullet_speed_upgrade += 1
+        if event == TWO and eru.gold >= (eru.speed_upgrade_value + 1) * 10:
+            if eru.speed_upgrade_value < 4:
+                eru.gold -= (eru.speed_upgrade_value + 1) * 10
+                eru.speed_upgrade_value += 1
 
-        if event == THREE and eru.hp < 3 and eru.gold >= eru.heart_upgrade * 10:
-            eru.gold -= eru.heart_upgrade * 10
-            eru.heart_upgrade += 1
-            eru.hp += 1
+        if event == THREE and eru.remain_hp < 3 and eru.gold >= (eru.hp_upgrade_value + 1) * 10:
+            eru.gold -= (eru.hp_upgrade_value + 1) * 10
+            eru.hp_upgrade_value += 1
+            eru.remain_hp += 1
 
         if event == ZERO:
-            dragons = main_state.get_dragons()
-            ground = main_state.get_ground()
-            if eru.stage_level < 4:
-                ground.stage_level += 1
-                eru.stage_level += 1
-                for dragon in dragons:
-                    if dragon.stage_level < 4:
-                        dragon.stage_level = ground.stage_level
+            eru.distance += 5000
 
         if event == NINE:
             eru.gold += 100
@@ -90,18 +84,18 @@ class MoveState:
                     % FRAMES_PER_ACTION
         eru.x += eru.velocity * game_framework.frame_time
         eru.x = clamp(Eru_Size * 0.25, eru.x, game_world.WIDTH - Eru_Size * 0.25)
-        eru.distance += game_framework.frame_time * game_world.SPEED_PPS
+        eru.distance += game_framework.frame_time * SPEED_PPS
 
         eru.bullet_timer += game_framework.frame_time * 10
 
-        if int(eru.bullet_timer) >= 3 - eru.bullet_speed_upgrade * 0.5:
+        if int(eru.bullet_timer) >= 3 - eru.speed_upgrade_value * 0.5:
             eru.bullet_shoot()
             eru.bullet_timer = 0
 
-        if eru.hptimer > 0:
-            eru.hptimer -= game_framework.frame_time
+        if eru.crash_effect_timer > 0:
+            eru.crash_effect_timer -= game_framework.frame_time
 
-        if eru.hp <= 0:
+        if eru.remain_hp <= 0:
             game_framework.change_state(title_state)
 
         dragons = main_state.get_dragons()
@@ -116,31 +110,14 @@ class MoveState:
     @staticmethod
     def draw(eru):
         eru.image.clip_draw(int(eru.frame) * 150, 0, 150, 150, eru.x, eru.y, Eru_Size, Eru_Size)
+
         eru.hp_draw()
-        eru.font.draw(game_world.WIDTH * 0.7, game_world.HEIGHT - 50,
-                      '%10.0f M' % (eru.distance - (eru.distance % 10)), (255, 255, 0))
-
-        eru.gold_image.draw(game_world.WIDTH * 0.05, game_world.HEIGHT - 50, 40, 40)
-        eru.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 50,
-                      ': %1.0f' % (eru.gold), (255, 255, 0))
-
-        eru.atk_image.draw(game_world.WIDTH * 0.05, game_world.HEIGHT - 100, 40, 40)
-        eru.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 100,
-                      ': %1.0f' % (eru.bullet_atk_upgrade), (255, 255, 0))
-
-        eru.speed_image.draw(game_world.WIDTH * 0.05, game_world.HEIGHT - 150, 40, 40)
-        eru.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 150,
-                      ': %1.0f' % (eru.bullet_speed_upgrade), (255, 255, 0))
-
-        eru.heart_image.clip_draw(120, 0, 128, 128, game_world.WIDTH * 0.05 + 7, game_world.HEIGHT - 200, 50, 50)
-        eru.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 200,
-                      ': %1.0f' % (eru.heart_upgrade), (255, 255, 0))
+        eru.distance_draw()
+        eru.gold_draw()
+        eru.upgrade_draw()
+        eru.crash_draw()
 
         draw_rectangle(*eru.get_bb())
-
-        if 0.4 <= eru.hptimer <= 0.6 or 0.8 <= eru.hptimer <= 1:
-            eru.damage_image.draw(eru.x + 50, eru.y + 70)
-            eru.damage_image2.draw(game_world.WIDTH * 0.5, game_world.HEIGHT * 0.5, game_world.WIDTH, game_world.HEIGHT)
 
 
 next_state_table = {
@@ -152,46 +129,64 @@ next_state_table = {
 
 
 class Eru:
+    # eru 관련
     image = None
+    gold_image = None
+
+    # eru 업그레이드 관련
+    attack_upgrade_image = None
+    speed_upgrade_image = None
+    hp_upgrade_image = None
+
+    # hp 관련
     Full_image = None
     Empty_image = None
-    damage_image = None
-    damage_image2 = None
-    gold_image = None
-    atk_image = None
-    speed_image = None
-    heart_image = None
+
+    # 피격 관련
+    eru_crash_image = None
+    background_crash_image = None
 
     def __init__(self):
         if Eru.image == None:
+            # eru 관련
             Eru.image = load_image('Eru.png')
-            Eru.Full_image = load_image('hp_heart_01.png')
-            Eru.Empty_image = load_image('hp_heart_02.png')
-            Eru.damage_image = load_image('damage.png')
-            Eru.damage_image2 = load_image('damage2.png')
             Eru.gold_image = load_image('gold.png')
-            Eru.atk_image = load_image('atk.png')
-            Eru.speed_image = load_image('speed.png')
-            Eru.heart_image = load_image('heart.png')
 
+            # eru 업그레이드 관련
+            Eru.attack_upgrade_image = load_image('attack_upgrade.png')
+            Eru.speed_upgrade_image = load_image('speed_upgrade.png')
+            Eru.hp_upgrade_image = load_image('hp_upgrade.png')
+
+            # hp 관련
+            Eru.Full_image = load_image('full_hp.png')
+            Eru.Empty_image = load_image('empty_hp.png')
+
+            # 피격 관련
+            Eru.eru_crash_image = load_image('eru_crush.png')
+            Eru.background_crash_image = load_image('background_crush.png')
+
+        # eru 위치 관련
+        self.x = game_world.WIDTH * 0.5
+        self.y = 100
         self.velocity = 0
         self.frame = 0
+
+        # eru 공격 관련
         self.bullet_timer = 0
-        self.x = WIDTH * 0.5
-        self.y = 100
-        self.hp = 3
-        self.hptimer = 0
+        self.attack_upgrade_value = 0
+        self.speed_upgrade_value = 0
+        self.hp_upgrade_value = 0
+
+        # eru HP 관련
+        self.remain_hp = 3
+        self.crash_effect_timer = 0
+
+        # eru 스테이지 관련
         self.distance = 0
         self.gold = 0
-
         self.stage_level = 0
 
-        self.bullets = []
-        self.bullets_number = 0
-        self.bullet_atk_upgrade = 1
-        self.bullet_speed_upgrade = 1
-        self.heart_upgrade = 1
-
+        # 그 외
         self.event_que = []
         self.cur_state = MoveState
         self.cur_state.enter(self, None)
@@ -201,7 +196,8 @@ class Eru:
         return self.x - Eru_Size * 0.2, self.y - Eru_Size * 0.25, self.x + Eru_Size * 0.2, self.y + Eru_Size * 0.25
 
     def bullet_shoot(self):
-        self.bullets += [EruBullet()]
+        bullets = main_state.get_bullets()
+        bullets += [EruBullet()]
 
     def add_event(self, event):
         self.event_que.insert(0, event)
@@ -223,8 +219,32 @@ class Eru:
             self.add_event(key_event)
 
     def hp_draw(self):
-        [self.Full_image.draw(325 + 50 * i, game_world.HEIGHT - 50) for i in range(self.hp)]
-        [self.Empty_image.draw(325 + 50 * i, game_world.HEIGHT - 50) for i in range(self.hp, 3)]
+        [self.Full_image.draw(325 + 50 * i, game_world.HEIGHT - 50) for i in range(self.remain_hp)]
+        [self.Empty_image.draw(325 + 50 * i, game_world.HEIGHT - 50) for i in range(self.remain_hp, 3)]
 
-    def get_bullets(self):
-        return self.bullets
+    def gold_draw(self):
+        self.gold_image.draw(game_world.WIDTH * 0.05, game_world.HEIGHT - 50, 40, 40)
+        self.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 50, ': %1.0f' % (self.gold), (255, 255, 0))
+
+    def upgrade_draw(self):
+        self.attack_upgrade_image.draw(game_world.WIDTH * 0.05, game_world.HEIGHT - 100, 40, 40)
+        self.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 100,
+                       ': %1.0f' % self.attack_upgrade_value, (255, 255, 0))
+
+        self.speed_upgrade_image.draw(game_world.WIDTH * 0.05, game_world.HEIGHT - 150, 40, 40)
+        self.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 150,
+                       ': %1.0f' % self.speed_upgrade_value, (255, 255, 0))
+
+        self.hp_upgrade_image.clip_draw(120, 0, 128, 128, game_world.WIDTH * 0.05 + 7, game_world.HEIGHT - 200, 50, 50)
+        self.font.draw(game_world.WIDTH * 0.1, game_world.HEIGHT - 200,
+                       ': %1.0f' % self.hp_upgrade_value, (255, 255, 0))
+
+    def distance_draw(self):
+        self.font.draw(game_world.WIDTH * 0.7, game_world.HEIGHT - 50,
+                       '%10.0f M' % (self.distance - (self.distance % 10)), (255, 255, 0))
+
+    def crash_draw(self):
+        if 0.4 <= self.crash_effect_timer <= 0.6 or 0.8 <= self.crash_effect_timer <= 1:
+            self.eru_crash_image.draw(self.x + 50, self.y + 70)
+            self.background_crash_image.draw(game_world.WIDTH * 0.5, game_world.HEIGHT * 0.5,
+                                             game_world.WIDTH, game_world.HEIGHT)
